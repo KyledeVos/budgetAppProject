0f05fc74c5f5e8f9e827958c81029f57e38802e8
KyledeVos <57043473+KyledeVos@users.noreply.github.com>
2022-05-20 00:00:53 +0200

    Modified Add Methods to return int id of new entry. Added method to return id of last entry in list - Needed for Look Up LinkedLists

--- src/budgetApp/Data/AddDataToDataBase.java
+++ src/budgetApp/Data/AddDataToDataBase.java
@@ -38,13 +38,27 @@ public class AddDataToDataBase {
 
     //Method to add new user to database
     //@params Strings user: name, last_name and email;
-    public void addUser(String first_name, String last_name, String email){
+    //@returns user_id after addition of user for addition to linkedList
+    public int addUser(String first_name, String last_name, String email){
+
+        int user_id = 0;
+
         try {
             preparedStatement = connection.prepareStatement("INSERT INTO users(first_name, last_name, email) VALUES(?, ? ,?)");
             preparedStatement.setString(1, first_name);
             preparedStatement.setString(2, last_name);
             preparedStatement.setString(3, email);
             preparedStatement.executeUpdate();
+
+            //get id of new_user
+            preparedStatement = connection.prepareStatement("SELECT * FROM users ORDER BY id DESC LIMIT 1");
+            resultSet = preparedStatement.executeQuery();
+
+            while(resultSet.next()){
+                user_id = resultSet.getInt("id");
+            }
+
+
         } catch(SQLException e){
             System.out.println(e.getMessage());
         } finally {
@@ -55,11 +69,18 @@ public class AddDataToDataBase {
                 e.printStackTrace();
             }
         }
+
+        return user_id;
     }
 
     //Method to add new incomes to database and update user_income table
     //@params user_id source_name, amount, payment_date, payment_interval, notes
-    public void addIncome(int user_id, String source_name, double amount, String payment_date, String payment_interval, String notes){
+    //@returns income_id after addition of income for addition to linkedList
+    public int addIncome(int user_id, String source_name, double amount, String payment_date, String payment_interval, String notes){
+
+        //id of new income after it is added for return to add income to linkedList
+        int income_id = 0;
+
         try {
 
             //insert data into database table income
@@ -72,15 +93,7 @@ public class AddDataToDataBase {
             preparedStatement.setString(5, notes);
             preparedStatement.executeUpdate();
 
-            int income_id = 0;
-
-            //get id from last insert (new income)
-            preparedStatement = connection.prepareStatement("SELECT * FROM income ORDER BY id DESC LIMIT 1");
-            resultSet = preparedStatement.executeQuery();
-
-            while(resultSet.next()){
-                income_id = resultSet.getInt("id");
-            }
+            income_id = idOfLastElement("income");
 
             //update lookup table with user_id and income_id
             preparedStatement = connection.prepareStatement("INSERT INTO user_income(user_id, income_id) VALUES(? , ?)");
@@ -98,11 +111,18 @@ public class AddDataToDataBase {
                 e.printStackTrace();
             }
         }
+
+        return income_id;
     }
 
     //Method to add new expenses to database and update user_expense table
     //@params user_id, type_category, paid_to, amount, payment_date, description, notes
-    public void addExpenses(int user_id, String type_category, String paid_to,  double amount, String payment_date, String description, String notes){
+    //@returns expense_id after addition of expense for addition to linkedList
+    public int addExpenses(int user_id, String type_category, String paid_to,  double amount, String payment_date, String description, String notes){
+
+        //id of new expense after it is added for return to add expense to linkedList
+        int expense_id = 0;
+
         try {
             preparedStatement = connection.prepareStatement("INSERT INTO expenses(type_category, paid_to, amount, payment_date, description, notes)" +
                     " VALUES(?, ? ,?, ?, ? , ?)");
@@ -114,15 +134,9 @@ public class AddDataToDataBase {
             preparedStatement.setString(6, notes);
             preparedStatement.executeUpdate();
 
-            int expense_id = 0;
 
             //get id from last insert (new expense)
-            preparedStatement = connection.prepareStatement("SELECT * FROM expenses ORDER BY id DESC LIMIT 1");
-            resultSet = preparedStatement.executeQuery();
-
-            while(resultSet.next()){
-                expense_id = resultSet.getInt("id");
-            }
+            expense_id = idOfLastElement("expenses");
 
             //update lookup table with user_id and expenses_id
             preparedStatement = connection.prepareStatement("INSERT INTO user_expenses(user_id, expenses_id) VALUES(? , ?)");
@@ -140,13 +154,20 @@ public class AddDataToDataBase {
                 e.printStackTrace();
             }
         }
+
+        return expense_id;
     }
 
 
     //Method to add new debt_payments to database and update user_debt_payments table
     //@params user_id type_category, paid_to, amount, payment_date, end_date, total_owed, interest, notes
-    public void addDebtExpenses(int user_id, String type_category, String paid_to,  double amount, String payment_date, String end_date, double total_owed
+    //@returns debt_payment_id after addition of debt_payment for addition to linkedList
+    public int addDebtExpenses(int user_id, String type_category, String paid_to,  double amount, String payment_date, String end_date, double total_owed
                                 , double interest, String notes){
+
+        //id of new debt_payments after it is added for return to add debt_payment to linkedList
+        int debt_payments_id = 0;
+
         try {
             preparedStatement = connection.prepareStatement("INSERT INTO debt_payments(type_category, paid_to, amount, " +
                     "payment_date, end_date, total_owed, interest, notes)" +
@@ -161,18 +182,15 @@ public class AddDataToDataBase {
             preparedStatement.setString(8, notes);
             preparedStatement.executeUpdate();
 
-            int debt_payments_id = 0;
-
             //get id from last insert (new debt_payment)
             preparedStatement = connection.prepareStatement("SELECT * FROM debt_payments ORDER BY id DESC LIMIT 1");
             resultSet = preparedStatement.executeQuery();
 
-            while(resultSet.next()){
-                debt_payments_id = resultSet.getInt("id");
-            }
+            debt_payments_id = idOfLastElement("debt_payments");
 
             //update lookup table with user_id and debt_payments_id
-            preparedStatement = connection.prepareStatement("INSERT INTO user_debt_payments(user_id, debt_payments_id) VALUES(? , ?)");
+            preparedStatement = connection.prepareStatement("INSERT INTO user_debt_payments(user_id, debt_payments_id) " +
+                    "VALUES(?,?)");
             preparedStatement.setInt(1, user_id);
             preparedStatement.setInt(2, debt_payments_id);
             preparedStatement.executeUpdate();
@@ -187,12 +205,17 @@ public class AddDataToDataBase {
                 e.printStackTrace();
             }
         }
+        return debt_payments_id;
     }
 
 
     //Method to add new savings to database and update user_savings table
     //@params user_id, saved_location, amount, saved_date, notes
-    public void addSavings(int user_id, String saved_location, double amount, String saved_date, String notes){
+    //@returns savings_id after addition of savings for addition to linkedList
+    public int addSavings(int user_id, String saved_location, double amount, String saved_date, String notes){
+
+        int savings_id = 0;
+
         try {
             preparedStatement = connection.prepareStatement("INSERT INTO savings(saved_location, amount, " +
                     "saved_date, notes)" +
@@ -203,15 +226,9 @@ public class AddDataToDataBase {
             preparedStatement.setString(4, notes);
             preparedStatement.executeUpdate();
 
-            int savings_id = 0;
 
             //get id from last insert (new savings)
-            preparedStatement = connection.prepareStatement("SELECT * FROM savings ORDER BY id DESC LIMIT 1");
-            resultSet = preparedStatement.executeQuery();
-
-            while(resultSet.next()){
-                savings_id = resultSet.getInt("id");
-            }
+            savings_id = idOfLastElement("savings");
 
             //update lookup table with user_id and savings_id
             preparedStatement = connection.prepareStatement("INSERT INTO user_savings(user_id, savings_id) VALUES(? , ?)");
@@ -229,13 +246,19 @@ public class AddDataToDataBase {
                 e.printStackTrace();
             }
         }
+
+        return savings_id;
     }
 
 
     //Method to add new custom_goal to database and update user_custom_goals table
     //@params user_id, description, saved_location, amount, saved_date, total_desired, final_date, notes
-    public void addCustomGoal(int user_id, String description, String saved_location,  double amount, String saved_date, double total_desired,
+    //@returns custom_goal_id after addition of custom_goal for addition to linkedList
+    public int addCustomGoal(int user_id, String description, String saved_location,  double amount, String saved_date, double total_desired,
                               String final_date, String notes){
+
+        int custom_goals_id = 0;
+
         try {
             preparedStatement = connection.prepareStatement("INSERT INTO custom_goals(description, saved_location, amount, saved_date, " +
                     "total_desired, final_date, notes)" +
@@ -249,15 +272,7 @@ public class AddDataToDataBase {
             preparedStatement.setString(7, notes);
             preparedStatement.executeUpdate();
 
-            int custom_goals_id = 0;
-
-            //get id from last insert (new custom_goal payment)
-            preparedStatement = connection.prepareStatement("SELECT * FROM custom_goals ORDER BY id DESC LIMIT 1");
-            resultSet = preparedStatement.executeQuery();
-
-            while(resultSet.next()){
-                custom_goals_id = resultSet.getInt("id");
-            }
+            custom_goals_id = idOfLastElement("custom_goals");
 
             //update lookup table with user_id and savings_id
             preparedStatement = connection.prepareStatement("INSERT INTO user_custom_goals(user_id, custom_goals_id) VALUES(? , ?)");
@@ -275,5 +290,29 @@ public class AddDataToDataBase {
                 e.printStackTrace();
             }
         }
+
+        return custom_goals_id;
+    }
+
+
+    //method to return id of last element in table
+    //@param name of table in data_base
+    //@return int id of last element in table
+    public int idOfLastElement(String tableName){
+
+        int id = 0;
+
+        try{
+            //get id from last element in specified table
+            preparedStatement = connection.prepareStatement("SELECT * FROM " + tableName + " ORDER BY id DESC LIMIT 1");
+            resultSet = preparedStatement.executeQuery();
+
+            while(resultSet.next()){
+                id = resultSet.getInt("id");
+            }
+        }catch(SQLException exc){
+            System.out.println(exc.getMessage());
+        }
+        return id;
     }
 }
