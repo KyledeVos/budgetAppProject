4bd40c796ff1114373ace721a5a70aa98b09a502
KyledeVos <57043473+KyledeVos@users.noreply.github.com>
2022-05-26 13:12:53 +0200

    Modified App Class to now use external method to validate user menu inputs and remove duplicated code. Re-order menu calls and options to always bring user back to main menu after sub-menu is completed. Began creating new sub menu to customize database

--- src/budgetApp/Controllers/AppClass.java
+++ src/budgetApp/Controllers/AppClass.java
@@ -2,19 +2,19 @@ package budgetApp.Controllers;
 
 import budgetApp.Data.ValidateData;
 import budgetApp.Services.AddData;
+import budgetApp.Services.ValidateUserInput;
 
 import java.util.Scanner;
 
 //Controller Class for Entire App
 public class AppClass {
 
+
+
     public static void main(String[] args) {
 
         //create scanner object used by program
         Scanner scanner = new Scanner(System.in);
-
-        //boolean to check if user wants to exit app
-        boolean quit = false;
 
         //user_id for current user
         int userId = 0;
@@ -23,70 +23,78 @@ public class AppClass {
 
         //first we check if there are any users in the database
 
-        //testing
         ValidateData validateData = new ValidateData();
-
         boolean noUsers = validateData.emptyUserTable();
 
         //if user table is not empty, then we want app User to select user from the database
         if(!noUsers){
             userId = validateData.validateUser(scanner);
-            printMainMenu();
         } else {
 
             //if user_table is empty then we first need the app user to add a new user to the database
             System.out.println("There are no users in App.");
-            AddData addData = new AddData();
+            AddData addData = new AddData(scanner);
 
             //assign userId to id of user just added
             userId = addData.addNewUser(scanner);
         }
 
-
-        //TEST
-        System.out.println("\nTEST\n");
-        AddData addData = new AddData();
-        addData.addNewIncome(userId, scanner);
+        //from here we begin to cycle through the menu options
+        mainMenu(userId, scanner);
 
 
-        //create infinite loop to cycle through menu options until user selects 'exit'
-//        while(!quit){
-//
-//
-//            boolean hasInt = scanner.hasNextInt();
-//
-//            //if user enters an int value, menuChoiceMethod() Evaluated
-//            if(hasInt){
-//                int menuChoice = scanner.nextInt();
-//                quit = mainMenuChoice(menuChoice);
-//            } else {
-//                //occurs if user did not enter an integer
-//                System.out.println("Invalid input. Please enter valid number between 1 and 5\n");
-//                //printMainMenu();
-//            }
-//
-//            //clear scanner input
-//            scanner.nextLine();
-//
-//        }
+//        //TEST
+//        System.out.println("\nTEST\n");
+//        AddData addData = new AddData(scanner);
+//        addData.addNewUser(scanner);
+//        addData.addNewIncome(userId, scanner);
+//        addData.addNewExpense(userId, scanner);
+
 
         //finally, close scanner at the end
         scanner.close();
+    }
 
+    private static void mainMenu(int userID , Scanner scanner){
+
+        printMainMenu();
+
+        //boolean to check if user wants to exit app
+        boolean quit = false;
+
+        //object allowing User input to be received and validated for menu options
+        ValidateUserInput validateUserInput = new ValidateUserInput(scanner);
+
+        //create infinite loop to cycle through menu options until user selects 'exit'
+
+        while(!quit){
+
+            int choice = validateUserInput.getMenuChoice(5);
+            quit = mainMenuChoice(choice, scanner);
+            if(!quit){
+                printMainMenu();
+            }
+
+        }
 
     }
 
+    //---------------------------------------------------------------------
+    //Main Menu
+
     private static void printMainMenu(){
         System.out.println("----------------------------------------");
+        System.out.println("MAIN MENU");
         System.out.println("\nPlease select a menu option below by entering the number next to your choice\n");
         System.out.println("1) View Account Balances");
         System.out.println("2) View Overview Stats");
         System.out.println("3) Customize Entries in Budget - Add, Remove or Update");
         System.out.println("4) Run Reports");
         System.out.println("5) Exit App");
+        System.out.println("----------------------------------------");
     }
 
-    private static boolean mainMenuChoice(int option){
+    private static boolean mainMenuChoice(int option, Scanner scanner){
 
         //used to return if user has chosen to exit app
         boolean quit = false;
@@ -101,7 +109,7 @@ public class AppClass {
                 break;
 
             case 3:
-                System.out.println("Customize entries in development");
+                customizeEntriesMenu(scanner);
                 break;
 
             case 4:
@@ -121,5 +129,65 @@ public class AppClass {
         return quit;
 
     }
+
+    /////////////////////////////////////////////////////////////
+    //CustomizeEntries Menu
+
+    //print menu options
+    private static void printCustomizeEntriesMenu(){
+
+        System.out.println("\nPlease select a menu option below by entering the number next to your choice\n");
+        System.out.println("1) Add new Entry ");
+        System.out.println("2) Remove an Entry");
+        System.out.println("3) Update/Change a current Entry");
+    }
+
+    private static boolean customizeEntriesMenu(Scanner scanner){
+
+        System.out.println("\n---------------------------");
+        System.out.println("Customize Entries Menu");
+        printCustomizeEntriesMenu();
+
+        ValidateUserInput validateUserInput = new ValidateUserInput(scanner);
+
+        //variable to hold user choice
+        int choice = validateUserInput.getMenuChoice(3);
+        customizeEntryChoice(choice);
+
+        //must return true to allow while loop to continue in main menu
+        return true;
+    }
+
+    private static void customizeEntryChoice(int choice){
+
+
+        switch(choice){
+
+            case 1:
+                System.out.println("\n------------------------");
+                System.out.println("Add new Entry Menu");
+                System.out.println("Additional Menu Options In development");
+                break;
+
+            case 2:
+                System.out.println("\n------------------------");
+                System.out.println("Remove Entry Menu");
+                System.out.println("Additional Menu Options In development");
+                break;
+
+            case 3:
+                System.out.println("\n------------------------");
+                System.out.println("Update Current Entry Menu");
+                System.out.println("Additional Menu Options In development");
+                break;
+
+            default:
+                System.out.println("Invalid");
+                break;
+        }
+
+    }
+
+
 
 }
